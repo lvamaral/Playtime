00b7fb34d95101f517653ff8907c0e64e0d843fb
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-06-04 18:17:32 -0700

    Add FB Auth

--- navigation/Router.js
+++ navigation/Router.js
@@ -4,10 +4,12 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import RootNavigation from './RootNavigation';
+import LoginScreen from '../screens/LoginScreen';
 
 export default createRouter(() => ({
   home: () => HomeScreen,
   links: () => LinksScreen,
   settings: () => SettingsScreen,
   rootNavigation: () => RootNavigation,
+  login: () => LoginScreen
 }));
