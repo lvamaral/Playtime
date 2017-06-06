d87adae74876b3fe1e5a496b7c7fe1cfc8391fc8
ianhoffman <ianhoffman10@gmail.com>
2017-06-06 11:05:14 -0700

    refactor get parks script to use radar search, getting more results

--- api/parks_server/postParksToFirebase.js
+++ api/parks_server/postParksToFirebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase';
 import secrets from './secrets2';
+var request = require('superagent');
 
 document.addEventListener('DOMContentLoaded', () => {
   var config = {
@@ -22,16 +23,43 @@ document.addEventListener('DOMContentLoaded', () => {
   const request = {
     location: sf,
     radius: '5000',
-    query: 'dog park'
+    keyword: 'dog park'
   };
 
   const service = new google.maps.places.PlacesService(map);
 
-  service.textSearch(request, (results, status) => {
+  service.radarSearch(request, (results, status) => {
     if (status === 'OK') {
-      debugger
+      for(let i = 0; i < 1; i++) {
+        (function (j) {
+          let request = {
+            placeId: results[i]['place_id']
+          };
+
+          var service = new google.maps.places.PlacesService(map);
+          setTimeout(function() {
+            service.getDetails(request, callback)
+          }, j * 1000);
+        })(i);
+
+        function callback(place, status) {
+          if(status === 'OK') {
+            let photoUrl = place.photos[0].getUrl({maxHeight: place.photos[0].height, maxWidth: place.photos[0].width});
+            let parkId = db.ref().child('parks').push().key;
+            db.ref('parks/' + parkId).set({
+              name: place.name,
+              address: place.formatted_address.slice(0, place.formatted_address.length - 15),
+              photoUrl: photoUrl,
+              lat: place.geometry.location.lat(),
+              lng: place.geometry.location.lng()
+            });
+          } else {
+            console.log(status);
+          }
+        }
+      }
     } else {
-      debugger
+      console.log(status);
     }
   });
 })
