a8b9d8962a3ff319db40060c61997b70237f7b94
ianhoffman <ianhoffman10@gmail.com>
2017-06-07 12:14:21 -0700

    merge to share parksview

--- components/dogs/DogIndexItem.js
+++ components/dogs/DogIndexItem.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableHighlight } from 'react-native';
+import { StackNavigation } from '@expo/ex-navigation';
 
 export default class DogIndexItem extends React.Component {
+  state = {
+    clicked: false
+  }
+
   render() {
     const { dog } = this.props;
 
-    return(
-      <View>
-        <Text>{dog.dogName}</Text>
-        <Text>{dog.age}</Text>
-        <Text>{dog.breed}</Text>
-        <Image source={{uri: dog.image}}
-               style={{width: 80, height: 80}} />
-      </View>
-    );
+    if(!this.state.clicked) {
+      return(
+        <TouchableHighlight onPress={() => this.setState({clicked: true})}>
+          <View>
+            <Text>{dog.dogName}</Text>
+            <Text>{dog.age}</Text>
+            <Text>{dog.breed}</Text>
+            <Image source={{uri: dog.image}}
+                    style={{width: 80, height: 80}} />
+          </View>
+        </TouchableHighlight>
+      );
+    } else {
+      return(
+        <View></View>
+      );
+    }
   }
 }
